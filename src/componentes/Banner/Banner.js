fb8ccae19e2bd08ada12c531e728bea5297131f9
Pedro Barros <devpedrobarros@hotmail.com>
2024-09-19 13:30:39 -0300

    first commit

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,10 @@
+import './Banner.css'
+
+export const Banner = () => {
+    // JSX
+    return (
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="o banner principal da página"/>
+        </header>
+    )
+}
